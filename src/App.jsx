3e73b65e287f85b6f0180f57c2be3ee883ee0ecb
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-12 12:11:14 +0100

    milestone1: create a button to fetch the API and print the names of the actresses

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import axios from "axios";
+import { useState } from "react";
+
 function App() {
+
+  const actressesEndpoint = "https://lanciweb.github.io/demo/api/actresses/";
+  const [actresses, setActresses] = useState([]);
+
+  function handleClick(){
+    axios
+      .get(actressesEndpoint)
+      .then(response => {
+        console.log(response.data);
+        setActresses(response.data);
+      })
+  }
 
   return (
     <>
-      
+      <h1>Booltiful</h1>
+      <button onClick={handleClick}>Clicca</button>
+      <ul>
+        {
+          actresses.map((actress) => (
+            <li key={actress.id}>{actress.name}</li>
+          ))
+        }
+      </ul>
     </>
   )
 }
