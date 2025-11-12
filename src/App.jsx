2039f5aea16a9fdc84abad7972f5a3c421f84382
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-12 17:37:48 +0100

    bonus1: add actors list

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from "react";
 function App() {
 
   const actressesEndpoint = "https://lanciweb.github.io/demo/api/actresses/";
+  const actorsEndpoint = "https://lanciweb.github.io/demo/api/actors/";
   const [actresses, setActresses] = useState([]);
+  const [actors, setActors] = useState([]);
 
   /* function handleClick(){
   } */
@@ -18,6 +20,15 @@ function App() {
       })
   }, [])
 
+  useEffect(() => {
+    axios
+      .get(actorsEndpoint)
+      .then(response => {
+        console.log(response.data);
+        setActors(response.data);
+      })
+  })
+
   return (
     <div className="container">
       <header className="text-center py-4">
@@ -25,10 +36,10 @@ function App() {
       </header>
 
       <ul className="list-unstyled">
-        <div className="row row-gap-3">
+        <div className="row row-gap-3 mb-4">
           {
             actresses.map((actress) => (
-              <div className="column col-12 col-md-3">
+              <div className="column col-12 col-md-6 col-lg-4">
                 <div className="card">
                   <li key={actress.id}>
                     <div className="image-container">
@@ -39,6 +50,27 @@ function App() {
                       <small>{actress.nationality}</small>
                       <p>{actress.biography}</p>
                       <p className="fst-italic">{actress.awards}</p>
+                    </div>
+                  </li>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+        <div className="row row-gap-3">
+          {
+            actors.map((actor) => (
+              <div className="column col-12 col-md-6 col-lg-4">
+                <div className="card">
+                  <li key={actor.id}>
+                    <div className="image-container">
+                      <img src={actor.image} className="card-img-top actress-image"/>
+                    </div>
+                    <div className="card-body">
+                      <h6>{actor.name} - {actor.birth_year}</h6>
+                      <small>{actor.nationality}</small>
+                      <p>{actor.biography}</p>
+                      <p className="fst-italic">{actor.awards}</p>
                     </div>
                   </li>
                 </div>
