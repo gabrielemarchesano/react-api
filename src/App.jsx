115be1a0ca81fde4f77947e099e40d95197c7e3a
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-12 12:25:47 +0100

    milestone1: add useEffect hook for API request to work when page load

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,38 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
 
   const actressesEndpoint = "https://lanciweb.github.io/demo/api/actresses/";
   const [actresses, setActresses] = useState([]);
 
-  function handleClick(){
+  /* function handleClick(){
+  } */
+  
+  useEffect(() => {
     axios
       .get(actressesEndpoint)
       .then(response => {
         console.log(response.data);
         setActresses(response.data);
       })
-  }
+  }, [])
 
   return (
     <>
       <h1>Booltiful</h1>
-      <button onClick={handleClick}>Clicca</button>
+      {/* <button onClick={handleClick}>Clicca</button> */}
       <ul>
         {
           actresses.map((actress) => (
-            <li key={actress.id}>{actress.name}</li>
+            <li key={actress.id}>
+              <img src={actress.image}/>
+              <p>{actress.name}</p>
+              <p>{actress.birth_year}</p>
+              <p>{actress.nationality}</p>
+              <p>{actress.biography}</p>
+              <p>{actress.awards}</p>
+            </li>
           ))
         }
       </ul>
